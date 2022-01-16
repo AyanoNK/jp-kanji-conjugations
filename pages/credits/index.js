4ff67d17cf98747f994be25cdd533802e9d77cec
Ayano <s.correa.wilches@gmail.com>
2022-01-16 16:49:09 +0700

    Added titling to credits

--- pages/credits/index.js
+++ pages/credits/index.js
@@ -1,11 +1,27 @@
-import { List, ListItem, ListIcon, Text, Flex, Link } from "@chakra-ui/react";
-import { ArrowForwardIcon, ExternalLinkIcon } from "@chakra-ui/icons";
+import {
+  List,
+  ListItem,
+  ListIcon,
+  Text,
+  Flex,
+  Link,
+  Heading,
+  Container,
+  VStack,
+  StackItem,
+} from "@chakra-ui/react";
+import {
+  ArrowForwardIcon,
+  ExternalLinkIcon,
+  ChevronRightIcon,
+} from "@chakra-ui/icons";
 
 export default function FirstPost() {
   const rows = [
     {
       id: "1",
       item: "Cat Logo",
+      who: "neko.dearu",
       links: [
         {
           id: "1.1",
@@ -17,28 +33,44 @@ export default function FirstPost() {
   ];
 
   return (
-    <Flex p={10}>
-      <List spacing={5}>
-        {rows.map((row) => (
-          <ListItem key={row.id}>
-            <Text fontSize="xl">
-              <ListIcon as={ArrowForwardIcon} color="green.500" />
-              {row.item}
-            </Text>
-            <List pl={7} pt={2} spacing={3}>
-              {row.links.map((link) => (
-                <ListItem>
-                  <Text fontSize="lg">
-                    <Link href={link.url} isExternal>
-                      <ExternalLinkIcon color="green.500" /> {link.title}
-                    </Link>
+    <Container maxW="6xl">
+      <VStack pt={5} spacing={5} align="stretch">
+        <StackItem>
+          <Heading
+            as="h1"
+            size="lg"
+            letterSpacing={"tighter"}
+            flexDirection="column"
+          >
+            Credits
+          </Heading>
+        </StackItem>
+        <StackItem>
+          <Flex>
+            <List spacing={5}>
+              {rows.map((row) => (
+                <ListItem key={row.id}>
+                  <Text fontSize="xl">
+                    <ListIcon as={ArrowForwardIcon} color="green.500" />
+                    {row.item} <ChevronRightIcon /> {row.who}
                   </Text>
+                  <List pl={7} pt={2} spacing={3}>
+                    {row.links.map((link) => (
+                      <ListItem>
+                        <Text fontSize="lg">
+                          <Link href={link.url} isExternal>
+                            <ExternalLinkIcon color="green.500" /> {link.title}
+                          </Link>
+                        </Text>
+                      </ListItem>
+                    ))}
+                  </List>
                 </ListItem>
               ))}
             </List>
-          </ListItem>
-        ))}
-      </List>
-    </Flex>
+          </Flex>
+        </StackItem>
+      </VStack>
+    </Container>
   );
 }
